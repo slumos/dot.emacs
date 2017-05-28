a55d72ea269a7eeb35afa8282c3b4b62d9432185
Steven Lumos <steven@lumos.us>
2017-05-27 19:17:54 -0700

    cleanup

--- custom.el
+++ custom.el
@@ -5,9 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magithub ob-restclient restclient editorconfig all-the-icons-ivy avy undo-tree python-mode puppet-mode lsp-mode groovy-mode web-mode seeing-is-believing rspec-mode inf-ruby yaml-mode all-the-icons org-id ob-sh org-plus-contrib ivy-hydra swiper flx polymode spaceline-all-the-icons zoom-frm ws-butler which-key use-package spaceline smyx-theme paradox org-bullets magit exec-path-from-shell)))
+    (json-mode js2-mode magithub ob-restclient restclient editorconfig all-the-icons-ivy avy undo-tree python-mode puppet-mode lsp-mode groovy-mode web-mode seeing-is-believing rspec-mode inf-ruby yaml-mode all-the-icons org-id ob-sh org-plus-contrib ivy-hydra swiper flx polymode spaceline-all-the-icons zoom-frm ws-butler which-key use-package spaceline smyx-theme paradox org-bullets magit exec-path-from-shell)))
  '(paradox-automatically-star t)
- '(paradox-github-token t))
+ '(paradox-github-token t t))
    
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
