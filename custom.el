e951f5529b174d74868c65fa2b11e1d20bd7885f
Steven Lumos <slumos@salesforce.com>
2018-06-05 13:14:45 -0700

    turn off markdown/org table thing

--- custom.el
+++ custom.el
@@ -7,34 +7,40 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#fffefe" "#9d0000" "#006a00" "#0e1b00" "#6845138" "#840086" "#003567" "#494949"])
- '(auth-sources '(lastpass))
+ '(auth-sources (quote (lastpass)))
  '(circe-default-directory "~/local/circe")
- '(circe-default-ip-family 'ipv4)
+ '(circe-default-ip-family (quote ipv4))
  '(circe-default-part-message "")
  '(circe-default-quit-message "")
  '(circe-default-realname "slumos")
- '(circe-notifications-alert-style 'osx-notifier)
+ '(circe-notifications-alert-style (quote osx-notifier))
  '(circe-reduce-lurker-spam nil)
  '(circe-server-auto-join-default-type :after-nick)
  '(compilation-scroll-output t)
  '(custom-safe-themes
-   '("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default))
+   (quote
+    ("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default)))
  '(ein:jupyter-default-server-command "jupyter")
  '(fci-rule-color "#151515")
  '(hl-sexp-background-color "#060404")
  '(js-indent-level 2)
  '(org-agenda-files
-   '("~/Sync/Notes/CASE.org" "~/Sync/Notes/Inbox.org" "~/Sync/Notes/SLUMOS.org"))
+   (quote
+    ("/Users/slumos/Google Drive/Notes/DMP.org" "/Users/slumos/Google Drive/Notes/org-jira/PE.org" "/Users/slumos/.config/emacs/config.org" "/Users/slumos/Sync/OneDrive/ST/Notes/SLUMOS.org" "/Users/slumos/Sync/OneDrive/ST/Notes/CASE.org" "/Users/slumos/Sync/OneDrive/ST/Notes/Inbox.org")))
+ '(org-crypt-disable-auto-save (quote encrypt))
+ '(org-crypt-key "F59B171B695B4C66")
+ '(org-ellipsis " ↴")
  '(org-outline-path-complete-in-steps nil)
  '(org-yank-adjusted-subtrees t)
  '(package-selected-packages
-   '(company-terraform terraform-mode flower jedi apache-mode indent-tools atomic-chrome org-jira chruby ace-window edit-indirect magithub flycheck-pycheckers flycheck-rust flycheck-yamllint flymake-puppet flymake-ruby upstart-mode toml-mode ace-link company ob-ipython zoom ein uuidgen ox-jira ox-gfm ox-clip ace-mc dockerfile-mode lastpass focus lua-mode circe circe-notifications browse-at-remote orgit orgbox secretaria org-super-agenda outshine nginx-mode leuven-theme org-brain flycheck-demjson flycheck-demjsonlint markdown-mode wgrep vagrant-tramp ruby-hash-syntax dumb-jump multiple-cursors counsel-projectile counsel json-mode js2-mode ob-restclient restclient editorconfig all-the-icons-ivy avy puppet-mode lsp-mode groovy-mode seeing-is-believing rspec-mode inf-ruby all-the-icons org-id ob-sh org-plus-contrib ivy-hydra flx polymode spaceline-all-the-icons zoom-frm ws-butler spaceline smyx-theme paradox org-bullets magit exec-path-from-shell))
+   (quote
+    (org-crypt ivy-prescient grayscale-theme company-terraform terraform-mode flower jedi apache-mode indent-tools atomic-chrome org-jira chruby ace-window edit-indirect magithub flycheck-pycheckers flycheck-rust flycheck-yamllint flymake-puppet flymake-ruby upstart-mode toml-mode ace-link company ob-ipython zoom ein uuidgen ox-jira ox-gfm ox-clip ace-mc dockerfile-mode lastpass focus lua-mode circe circe-notifications browse-at-remote orgit orgbox secretaria org-super-agenda outshine nginx-mode leuven-theme org-brain flycheck-demjson flycheck-demjsonlint markdown-mode wgrep vagrant-tramp ruby-hash-syntax dumb-jump multiple-cursors counsel-projectile counsel json-mode js2-mode ob-restclient restclient editorconfig all-the-icons-ivy avy puppet-mode lsp-mode groovy-mode seeing-is-believing rspec-mode inf-ruby all-the-icons org-id ob-sh org-plus-contrib ivy-hydra flx polymode spaceline-all-the-icons zoom-frm ws-butler spaceline smyx-theme paradox org-bullets magit exec-path-from-shell)))
  '(paradox-automatically-star t)
  '(paradox-github-token t)
  '(sh-basic-offset 2)
  '(sh-indentation 2)
- '(sh-learn-basic-offset 'usually)
- '(tramp-syntax 'default nil (tramp))
+ '(sh-learn-basic-offset (quote usually))
+ '(tramp-syntax (quote default) nil (tramp))
  '(vc-follow-symlinks t))
 
 (custom-set-faces
@@ -42,4 +48,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-ellipsis ((t (:foreground "LightGoldenrod")))))
