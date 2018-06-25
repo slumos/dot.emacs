a1c0e66bc7b5a8d50a2b7ffed6717e642465227e
Steven Lumos <steven@lumos.us>
2018-06-25 16:16:18 -0700

    Add journal capture type

--- custom.el
+++ custom.el
@@ -7,37 +7,51 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#fffefe" "#9d0000" "#006a00" "#0e1b00" "#6845138" "#840086" "#003567" "#494949"])
- '(auth-sources '(lastpass))
+ '(auth-sources (quote (lastpass)))
  '(circe-default-directory "~/local/circe")
- '(circe-default-ip-family 'ipv4)
+ '(circe-default-ip-family (quote ipv4))
  '(circe-default-part-message "")
  '(circe-default-quit-message "")
  '(circe-default-realname "slumos")
- '(circe-notifications-alert-style 'osx-notifier)
+ '(circe-notifications-alert-style (quote osx-notifier))
  '(circe-reduce-lurker-spam nil)
  '(circe-server-auto-join-default-type :after-nick)
  '(compilation-scroll-output t)
  '(custom-safe-themes
-   '("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default))
+   (quote
+    ("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default)))
  '(ein:jupyter-default-server-command "jupyter")
  '(fci-rule-color "#151515")
  '(hl-sexp-background-color "#060404")
  '(js-indent-level 2)
  '(org-agenda-files
-   '("~/Sync/Notes/CASE.org" "~/Sync/Notes/Inbox.org" "~/Sync/Notes/SLUMOS.org"))
+   (quote
+    ("~/Sync/Notes/CASE.org" "~/Sync/Notes/Inbox.org" "~/Sync/Notes/SLUMOS.org")))
+ '(org-ellipsis " ↴")
+ '(org-file-apps
+   (quote
+    ((auto-mode . emacs)
+     ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default)
+     ("\\.pdf\\'" . emacs))))
  '(org-outline-path-complete-in-steps nil)
  '(org-yank-adjusted-subtrees t)
+ '(package-selected-packages
+   (quote
+    (multiple-cursors punpun-theme faff-theme nordless-theme org-beautify-theme quasi-monochrome-theme white-theme writeroom-mode terraform-mode chruby indent-tools atomic-chrome ace-link yaml-mode xterm-color ws-butler which-key web-mode vagrant-tramp use-package undo-tree spaceline-all-the-icons smyx-theme seeing-is-believing ruby-hash-syntax rspec-mode puppet-mode polymode paradox ox-jira ox-gfm ox-clip outshine orgit org-super-agenda org-plus-contrib org-bullets ob-restclient magithub lua-mode lsp-mode lastpass json-mode jedi inf-ruby groovy-mode flycheck-demjsonlint flx exec-path-from-shell ein editorconfig dumb-jump default-text-scale counsel-projectile copy-as-format browse-at-remote avy)))
  '(paradox-automatically-star t)
  '(paradox-github-token t)
  '(sh-basic-offset 2)
  '(sh-indentation 2)
- '(sh-learn-basic-offset 'usually)
- '(tramp-syntax 'default nil (tramp))
- '(vc-follow-symlinks t))
+ '(sh-learn-basic-offset (quote usually))
+ '(tramp-syntax (quote default) nil (tramp))
+ '(vc-follow-symlinks t)
+ '(writeroom-width 100))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:height 221))))
+ '(org-ellipsis ((t (:foreground "LightGoldenrod")))))
