a96db399416081a9119967f101bfd573e9fbef67
Steven Lumos <slumos@salesforce.com>
2018-09-13 11:22:18 -0700

    Pretty org-ellipsis

--- custom.el
+++ custom.el
@@ -42,4 +42,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180)))))
+ '(default ((t (:height 180))))
+ '(org-ellipsis ((t (:foreground "LightGoldenrod")))))
