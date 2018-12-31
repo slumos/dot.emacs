b02ef3bb5a9978460233b4560727a290ffd443f5
Steven Lumos <steven@lumos.us>
2018-12-31 11:15:07 -0800

    sync

--- custom.el
+++ custom.el
@@ -17,15 +17,17 @@
  '(compilation-scroll-output t)
  '(custom-safe-themes
    (quote
-    ("7cb89c93e37327cf1545fd8579816241446c611ebabc144a9835d09bbe132b19" "8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default)))
+    ("d88c43fe03ac912e35963695caf0ae54bc6ce6365c3a42da434ef639f7a37399" "7cb89c93e37327cf1545fd8579816241446c611ebabc144a9835d09bbe132b19" "8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "d5f17ae86464ef63c46ed4cb322703d91e8ed5e718bf5a7beb69dd63352b26b2" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "ed317c0a3387be628a48c4bbdb316b4fa645a414838149069210b66dd521733f" default)))
  '(ein:jupyter-default-server-command "jupyter")
  '(fci-rule-color "#151515")
  '(hl-sexp-background-color "#060404")
  '(js-indent-level 2)
+ '(line-spacing 0.2)
+ '(lua-indent-level 2)
  '(org-agenda-files
    (quote
     ("~/Sync/Notes/CASE.org" "~/Sync/Notes/Inbox.org" "~/Sync/Notes/SLUMOS.org")))
- '(org-ellipsis " 〉")
+ '(org-ellipsis " ⤵")
  '(org-file-apps
    (quote
     ((auto-mode . emacs)
@@ -36,7 +38,7 @@
  '(org-yank-adjusted-subtrees t)
  '(package-selected-packages
    (quote
-    (yaml-mode xterm-color ws-butler writeroom-mode white-theme which-key web-mode vagrant-tramp use-package undo-tree terraform-mode spaceline-all-the-icons solarized-theme smyx-theme seeing-is-believing ruby-hash-syntax rspec-mode rainbow-mode quasi-monochrome-theme puppet-mode punpun-theme polymode poet-theme pdf-tools paradox ox-jira ox-gfm ox-clip outshine orgit org-super-agenda org-plus-contrib org-doing org-bullets org-beautify-theme ob-restclient ob-async nordless-theme nord-theme noflet multiple-cursors magithub lua-mode lsp-mode load-theme-buffer-local lastpass json-mode jedi ivy-prescient inf-ruby indent-tools helm-org-rifle groovy-mode flycheck-demjsonlint flx faff-theme exec-path-from-shell ein editorconfig dumb-jump default-text-scale counsel-projectile copy-as-format chruby cask browse-at-remote atomic-chrome ace-link)))
+    (diminish focus all-the-icons-ivy olivetti forge yaml-mode xterm-color ws-butler writeroom-mode white-theme which-key web-mode vagrant-tramp use-package undo-tree terraform-mode spaceline-all-the-icons solarized-theme smyx-theme seeing-is-believing ruby-hash-syntax rspec-mode rainbow-mode quasi-monochrome-theme puppet-mode punpun-theme polymode poet-theme pdf-tools paradox ox-jira ox-gfm ox-clip outshine orgit org-super-agenda org-plus-contrib org-doing org-bullets org-beautify-theme ob-restclient ob-async nordless-theme nord-theme noflet multiple-cursors magithub lua-mode lsp-mode load-theme-buffer-local lastpass json-mode jedi ivy-prescient inf-ruby indent-tools helm-org-rifle groovy-mode flycheck-demjsonlint flx faff-theme exec-path-from-shell ein editorconfig dumb-jump default-text-scale counsel-projectile copy-as-format chruby cask browse-at-remote atomic-chrome ace-link)))
  '(paradox-automatically-star t)
  '(paradox-github-token t)
  '(sh-basic-offset 2)
@@ -50,4 +52,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((((class color) (min-colors 89)) (:foreground "#D8DEE9" :background "#2E3440")))))
