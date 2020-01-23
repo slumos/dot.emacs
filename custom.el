a96fab75fab51772bdaeb6906086742aa1a33427
Steven Lumos <slumos@salesforce.com>
2020-01-23 08:47:04 -0800

    Things:
    - start using posframe finally
    - fix server on macOS for reals??
    - neat time-stamp config

--- custom.el
+++ custom.el
@@ -80,6 +80,12 @@
      ("\\?\\?\\?+" . "#dc752f")))
  '(indent-tabs-mode nil)
  '(ivy-omni-org-file-sources nil t)
+ '(ivy-posframe-border-width 10)
+ '(ivy-posframe-display-functions-alist
+   '((swiper)
+     (swiper-isearch)
+     (t . ivy-posframe-display-at-frame-center)))
+ '(ivy-posframe-mode t nil (ivy-posframe))
  '(ivy-rich-mode t)
  '(ivy-rich-parse-remote-buffer nil)
  '(ivy-rich-switch-buffer-project-max-length 32)
@@ -94,7 +100,7 @@
  '(lsp-enable-completion-at-point t)
  '(lsp-log-io t)
  '(lsp-log-max 2000)
- '(lsp-ui-doc-enable t t)
+ '(lsp-ui-doc-enable t)
  '(lua-indent-level 2)
  '(magit-clone-always-transient nil)
  '(magit-clone-default-directory "~/Projects/")
@@ -104,10 +110,8 @@
  '(magit-file-mode t t)
  '(magit-repository-directories
    '(("~/Projects" . 1)
-     ("~/Projects/java" . 1)
-     ("~/Projects/puppet" . 1)
-     ("~/Projects/sfdc-slumos" . 1)
-     ("~/Projects/tf" . 1)))
+     ("~/Projects/iac" . 1)
+     ("~/Projects/puppet" . 1)))
  '(magit-wip-mode t)
  '(objed-cursor-color "#99324B")
  '(org-agenda-files
@@ -127,9 +131,10 @@
      ("elpy" . "https://jorgenschaefer.github.io/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-selected-packages
-   '(ein dap-mode ivy-rich doom-modeline ivy-posframe posframe yaml-tomato indent-guide org-ql ert-expectations vscdark-theme feather deft ivy-omni-org flycheck-pycheckers pyenv-mode-auto python-docstring python-mode python-pytest worf apples-mode ob-applescript paredit orgit straight nil smyx-theme dap-java nordless-theme ws-butler-mode fennel-mode mvn aws-snippets lsp-scala lsp-ui lush-theme company-lsp rustic flycheck-rust rust-mode counsel-dash poly-markdown flymake-jslint flymake-json lsp-javacomp org-plus-contrib org-edna ox-slack ox-slimhtml tao-yin-theme el-get leaf-keywords counsel-tramp wgrep iedit arjen-grey-theme goose-theme grayscale-theme klere-theme kosmos-theme spacegray-theme plan9-theme sexy-monochrome-theme rimero-theme brutalist-theme basic-theme poet-theme eink-theme eziam-theme tao-theme parchment-theme color-theme-sanityinc-solarized jq-mode gruvbox-theme exotica-theme hydandata-light-theme madhat2r-theme majapahit-theme minimal-theme nova-theme org-beautify-theme overcast-theme paper-theme professional-theme quasi-monochrome-theme snazzy-theme spacemacs-theme tangotango-theme white-theme json-reformatter-jq s3ed ein-notebook ob-async use-package-hydra hydra-use-package spray nose quelpa-use-package quelpa company-restclient go-mode web-mode terraform-mode chruby seeing-is-believing ruby-hash-syntax rspec-mode puppet-mode ox-jira ox-gfm ox-clip org-bullets indent-tools vagrant-tramp ivy-prescient ws-butler which-key ace-link spaceline-all-the-icons spaceline exec-path-from-shell forge flycheck-yamllint language-detection ox-confluence nord yaml-imenu constant-theme elpy flycheck-mode markdown-toc atomic-chrome browse-at-remote calfw calfw-cal calfw-gcal calfw-ical calfw-org counsel-org-capture-string default-text-scale spinner company-jedi copy-as-format company ob-ipython ace-mc lastpass focus lua-mode nginx-mode leuven-theme org-brain flycheck-demjson markdown-mode json-mode js2-mode ob-restclient editorconfig undo-tree all-the-icons org-id ob-sh ivy-hydra flx zoom-frm magit))
+   '(doom-themes lsp-ivy lsp-python-ms ein dap-mode ivy-rich doom-modeline ivy-posframe posframe yaml-tomato indent-guide org-ql ert-expectations vscdark-theme feather deft ivy-omni-org flycheck-pycheckers pyenv-mode-auto python-docstring python-mode python-pytest worf apples-mode ob-applescript paredit orgit straight nil smyx-theme dap-java nordless-theme ws-butler-mode fennel-mode mvn aws-snippets lsp-scala lsp-ui lush-theme company-lsp rustic flycheck-rust rust-mode counsel-dash poly-markdown flymake-jslint flymake-json lsp-javacomp org-plus-contrib org-edna ox-slack ox-slimhtml tao-yin-theme el-get leaf-keywords counsel-tramp wgrep iedit arjen-grey-theme goose-theme grayscale-theme klere-theme kosmos-theme spacegray-theme plan9-theme sexy-monochrome-theme rimero-theme brutalist-theme basic-theme poet-theme eink-theme eziam-theme tao-theme parchment-theme color-theme-sanityinc-solarized jq-mode gruvbox-theme exotica-theme hydandata-light-theme madhat2r-theme majapahit-theme minimal-theme nova-theme org-beautify-theme overcast-theme paper-theme professional-theme quasi-monochrome-theme snazzy-theme spacemacs-theme tangotango-theme white-theme json-reformatter-jq s3ed ein-notebook ob-async use-package-hydra hydra-use-package spray nose quelpa-use-package quelpa company-restclient go-mode web-mode terraform-mode chruby seeing-is-believing ruby-hash-syntax rspec-mode puppet-mode ox-jira ox-gfm ox-clip org-bullets indent-tools vagrant-tramp ivy-prescient ws-butler which-key ace-link spaceline-all-the-icons spaceline exec-path-from-shell forge flycheck-yamllint language-detection ox-confluence nord yaml-imenu constant-theme elpy flycheck-mode markdown-toc atomic-chrome browse-at-remote calfw calfw-cal calfw-gcal calfw-ical calfw-org counsel-org-capture-string default-text-scale spinner company-jedi copy-as-format company ob-ipython ace-mc lastpass focus lua-mode nginx-mode leuven-theme org-brain flycheck-demjson markdown-mode json-mode js2-mode ob-restclient editorconfig undo-tree all-the-icons org-id ob-sh ivy-hydra flx zoom-frm magit))
  '(paradox-automatically-star t)
  '(paradox-github-token t)
+ '(pdf-view-midnight-colors '("#fdf4c1" . "#32302f"))
  '(pos-tip-background-color "#F1EBDD")
  '(pos-tip-foreground-color "#011627")
  '(python-pytest-executable "pipenv run pytest")
@@ -143,6 +148,7 @@
  '(sml/active-foreground-color "#424242")
  '(sml/inactive-background-color "#4fa8a8")
  '(sml/inactive-foreground-color "#424242")
+ '(time-stamp-format "%Y-%02m-%02d %3a %02H:%02M %l@%q")
  '(tramp-syntax 'default nil (tramp))
  '(transient-default-level 5)
  '(transient-mark-mode nil)
@@ -181,5 +187,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "PragmataPro"))))
  '(fixed-pitch ((t (:family "PragmataPro")))))
