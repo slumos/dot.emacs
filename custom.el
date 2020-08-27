84c4424c3a72ee6f18c19786bf673fc586ed611f
Steven Lumos <steven@lumos.us>
2020-08-27 16:45:44 -0700

    org tuning

--- custom.el
+++ custom.el
@@ -219,4 +219,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-ellipsis ((t (:foreground "LightGoldenrod" :underline nil)))))
