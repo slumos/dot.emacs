fa8ac5157331fe6d7dd76966aefd44ec3e2990c4
Steven Lumos <slumos@salesforce.com>
2022-04-12 18:50:40 -0700

    emacs 28.1 released yay

--- brew-emacs.sh
+++ brew-emacs.sh
@@ -1,7 +1,8 @@
-brew reinstall emacs-plus@28 \
+brew upgrade emacs-plus@28 \
      --verbose \
      --display-times \
      --with-native-comp \
      --with-no-frame-refocus \
      --with-xwidgets \
+     --with-imagemagick \
      --with-nobu417-big-sur-icon
