43c0f66888f95b934090137b22166a245a25316b
Steven Lumos <slumos@salesforce.com>
2022-08-09 15:26:39 -0700

    sync

--- brew-emacs.sh
+++ brew-emacs.sh
@@ -1,8 +1,26 @@
-brew upgrade emacs-plus@28 \
-     --verbose \
-     --display-times \
-     --with-native-comp \
-     --with-no-frame-refocus \
-     --with-xwidgets \
-     --with-imagemagick \
-     --with-nobu417-big-sur-icon
+#!/usr/bin/env zsh -f
+
+set -euo pipefail
+
+next-output-file-name () {
+  base="$1"
+  for ((i=0; i<10; i++)); do
+    filename="${base}.${i}"
+    [[ ! -e "$filename" ]] && print "$filename" && return
+  done
+  print "ERROR"
+}
+
+set -x
+
+logfile="$(next-output-file-name brew-emacs.out)"
+
+brew install \
+  d12frosted/emacs-plus/emacs-plus@28 \
+  --display-times \
+  --with-native-comp \
+  --with-no-frame-refocus \
+  --with-xwidgets \
+  --with-imagemagick \
+  --with-modern-nuvola-icon \
+| tee "$logfile"
