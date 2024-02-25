df8c001addc92e2f7fd7ea987001cbbb85f16f5b
Steven Lumos <steven@lumos.us>
2024-02-24 22:53:41 -0800

    lsp-doctor recommendations + sync

--- early-init.el
+++ early-init.el
@@ -7,3 +7,6 @@
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (expt 2 23))))
+
+;; Recommended by https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setenv "LSP_USE_PLISTS" "true")
